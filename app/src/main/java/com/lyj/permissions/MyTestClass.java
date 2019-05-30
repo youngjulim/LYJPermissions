5835782ff0b672f22affda8ef1a18650ff4d7a22
youngjulim <lyjdev@gmail.com>
2019-05-30 13:25:16 +0900

    손쉬운 퍼미션 요청 라이브러리

---
+++ app/src/main/java/com/lyj/permissions/MyTestClass.java
@@ -0,0 +1,11 @@
+package com.lyj.permissions;
+
+import android.util.Log;
+
+public class MyTestClass {
+
+    public static void libTest(String log){
+
+        Log.e("YJ", "Log : " + log);
+    }
+}
