39af3676a61adf50e7fad815b9f2094225aa0f3b
youngjulim <lyjdev@gmail.com>
2019-06-05 11:23:16 +0900

    주석처리 진행

--- app/src/main/java/com/lyj/permissions/LYJPermission.java
+++ app/src/main/java/com/lyj/permissions/LYJPermission.java
@@ -2,6 +2,9 @@ package com.lyj.permissions;
 
 public class LYJPermission {
 
+    /*
+            주석처리
+     */
     public static LYJPermissionConfig getConfig(){
 
         return LYJPermissionConfig.getInstance();
