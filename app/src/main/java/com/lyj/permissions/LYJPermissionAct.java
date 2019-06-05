b50a4a022b655376c5c35d7788d9678f4883ef25
youngjulim <lyjdev@gmail.com>
2019-06-05 17:24:25 +0900

    - 시스템 오버레이 권한 요청 로직 추가.

--- app/src/main/java/com/lyj/permissions/LYJPermissionAct.java
+++ app/src/main/java/com/lyj/permissions/LYJPermissionAct.java
@@ -1,8 +1,11 @@
 package com.lyj.permissions;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
@@ -18,18 +21,18 @@ import java.util.ArrayList;
 public class LYJPermissionAct extends AppCompatActivity{
 
     private int requestCode = 0;
+    private int REQUEST_CODE_SYSTEM_OVERLAY = 101;
     // 사용자에게 요구 권한 목록
     private String[] requestPermissions;
     // 사용자가 거부한 권한 목록
     private String[] rejectionPermissions;
-
-
+    // 시스템 오버레이 권한 요청 유무
+    private boolean isSystemOverlay = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         overridePendingTransition(0, 0);
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-        Log.e("YJ", "1234566711199990000009808018080880");
         GetConfig();
     }
 
@@ -37,6 +40,7 @@ public class LYJPermissionAct extends AppCompatActivity{
 
         this.requestCode = getIntent().getIntExtra(LYJPermission.REQUEST_CODE, 0);
         this.requestPermissions = getIntent().getStringArrayExtra(LYJPermission.REQUEST_PERMISSIONS);
+        this.isSystemOverlay =  getIntent().getBooleanExtra(LYJPermission.REQUEST_SYSTEM_OVERLAY, false);
         // 권한 요청
         RequestPermission();
     }
@@ -52,6 +56,7 @@ public class LYJPermissionAct extends AppCompatActivity{
             ActivityCompat.requestPermissions(this, tempPermissions, this.requestCode);
         }else{ // 권한이 모드 승인된 상태
             LYJPermissionConfig.getInstance().getPermissionDelegate().permissionCompleted(tempPermissions);
+            finish();
         }
     }
 
@@ -109,7 +114,12 @@ public class LYJPermissionAct extends AppCompatActivity{
                 }
                 // 사용자가 권한을 허용했을 경우.
                 if(isGranted){
-                    LYJPermissionConfig.getInstance().getPermissionDelegate().permissionCompleted(permissions);
+                    // 시스템 오버레이 권한 요청이 있다면
+                    if(isSystemOverlay){
+                        startOverlayWindowService();
+                    }else{
+                        LYJPermissionConfig.getInstance().getPermissionDelegate().permissionCompleted(permissions);
+                    }
                 }else{
                     // 사용자가 권한을 허용하지 않았을 경우.
                     LYJPermissionConfig.getInstance().getPermissionDelegate().permissionFailed(permissions);
@@ -117,5 +127,37 @@ public class LYJPermissionAct extends AppCompatActivity{
             }
         }
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_SYSTEM_OVERLAY){
+            if(checkCanOverlay()){
+                LYJPermissionConfig.getInstance().getPermissionDelegate().permissionCompleted(requestPermissions);
+            }else{
+                LYJPermissionConfig.getInstance().getPermissionDelegate().permissionFailed(requestPermissions);
+            }
+        }
+        finish();
+    }
+
+    /**
+     * 시스템 오버레이가 활성화되어 있는지 체크
+     * @return
+     */
+    protected boolean checkCanOverlay(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return Settings.canDrawOverlays(this);
+        }
+        return false;
+    }
+    /**
+     * 다른 앱 위에 그리기 퍼미션 체크
+     * 권한 체크되어 있지 않다면 권한 설정 화면으로 이동.
+     */
+    public void startOverlayWindowService() {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, REQUEST_CODE_SYSTEM_OVERLAY);
     }
 }
