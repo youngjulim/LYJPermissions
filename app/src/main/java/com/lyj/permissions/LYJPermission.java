e9aef3cd66489175ac2d529eadb2aca5fed6a950
youngjulim <lyjdev@gmail.com>
2019-06-05 17:23:53 +0900

    - REQUEST_SYSTEM_OVERLAY : 제어 상수 추가

--- app/src/main/java/com/lyj/permissions/LYJPermission.java
+++ app/src/main/java/com/lyj/permissions/LYJPermission.java
@@ -4,6 +4,7 @@ public class LYJPermission {
 
     public static final String REQUEST_CODE = "req_code";
     public static final String REQUEST_PERMISSIONS = "req_permission";
+    public static final String REQUEST_SYSTEM_OVERLAY = "req_system_overlay";
     /*
             주석처리!!!!!!
      */
